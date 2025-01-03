d796f0f66e3865a7355df2f07de6cb71b58ff573
blancks <s.rotondo90@gmail.com>
2025-01-03 03:54:57 +0100

    fix: removed CR from .env files

--- install.bat
+++ install.bat
@@ -94,6 +94,11 @@ SETLOCAL DISABLEDELAYEDEXPANSION
     call :FileReplaceValue "PMA_PORT=" "PMA_PORT=8901" %EnvFile%
     call :FileReplaceValue "MYSQL_DATABASE=" "MYSQL_DATABASE=gdrcd" %EnvFile%
 
+    :: rimuove eventuali CarriageReturn nel file .env
+    cd %InstallationFolder%
+    call wsl sed -i 's/\r//' .env
+    cd ..
+
     ::echo ErrorLevel: %ERRORLEVEL%
     :: rimuovo eventuali carriage return presenti nel file: non piace al sottosistema linux
     :: call wsl sed -i 's/\r//' ./stack/run
@@ -126,7 +131,9 @@ SETLOCAL DISABLEDELAYEDEXPANSION
 :::: Funzioni ::::
 
     :StackInstallAndStart
+        :: installa, builda i container e li avvia
         call wsl sudo ./stack/run install
+        call wsl ./stack/run build
         call wsl ./stack/run start
         exit /B 0
 
